e7ff4018834c2834b4eb0d6c4883affb2fa58936
Haseeb Nasee <haseebnaseem@gmail.com>
2018-07-13 04:33:42 +0500

    added a set access token method

--- android/src/main/java/com/paitoo/RNReactNativeSnapkitModule.java
+++ android/src/main/java/com/paitoo/RNReactNativeSnapkitModule.java
@@ -1,22 +1,43 @@
 
 package com.paitoo;
-
+import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
-
+import com.snapchat.kit.sdk.SnapLogin;
 public class RNReactNativeSnapkitModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
 
   public RNReactNativeSnapkitModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    this.reactContext = reactContext;
   }
 
   @Override
   public String getName() {
     return "RNReactNativeSnapkit";
+  }
+
+  @ReactMethod
+  public void onClick() {
+  		SnapLogin.getAuthTokenManager(getReactApplicationContext()).startTokenGrant();
+  }
+
+  @ReactMethod
+  public boolean isUserLoggedIn () {
+   		boolean isTrue = SnapLogin.isUserLoggedIn(getReactApplicationContext());
+        String str = Boolean.toString(isTrue);
+        Log.w("Helloooooooooooooooooo",str);
+        return isTrue;
+  }
+
+  @ReactMethod
+  public void revokeToken() {
+  	SnapLogin.getAuthTokenManager(getReactApplicationContext()).revokeToken();
+  }
+
+  @ReactMethod
+  public void setToken(String accessToken){
+    SnapLogin.getAuthTokenManager(getReactApplicationContext()).setAccessToken(accessToken);
   }
 }
